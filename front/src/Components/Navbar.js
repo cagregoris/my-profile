e76ca38d0de553cc5684e850c93df6c7a05d11ac
Carolyn Gregoris <carolyn.a.scobie@gmail.com>
2022-02-04 19:15:55 -0700

    responsive nav and part of intro

--- front/src/Components/Navbar.js
+++ front/src/Components/Navbar.js
@@ -1,31 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import './navbar.css'
 
 import {Link} from 'react-scroll';
 
 function Navbar() {
+
+  const [isMobile, setIsMobile] = useState(false)
+
   return (
     <div className="nav">
       <div className="logo">
         <Link to="/" spy={true} smooth={true}><a>CG.</a></Link>
       </div>
-      <div className="navLinks">
-        <ul>
-          <li>
-            <Link to="about" spy={true} smooth={true}><a>About</a></Link>
-          </li>
-          <li>
-            <Link to="skills" spy={true} smooth={true}><a>Skills</a></Link>
-          </li>
-          <li>
-          <Link to="projects" spy={true} smooth={true}><a>Projects</a></Link>
-          </li>
-          <li>
-          <Link to="contact" spy={true} smooth={true}><a>Contact</a></Link>
-          </li>
+
+      {/* <div className={isMobile ? "nav-links-mobile" : "nav-links"} onClick={() => setIsMobile(false)}> */}
+        <ul className={isMobile ? "nav-links-mobile" : "nav-links"} onClick={() => setIsMobile(false)}>
+         
+            <Link to="about" spy={true} smooth={true} onClick={() => setIsMobile(false)}><li>About</li></Link>
+         
+            <Link to="skills" spy={true} smooth={true} onClick={() => setIsMobile(false)}><li>Skills</li></Link>
+         
+          <Link to="projects" spy={true} smooth={true} onClick={() => setIsMobile(false)}><li>Projects</li></Link>
+          
+          <Link to="contact" spy={true} smooth={true} onClick={() => setIsMobile(false)}><li>Contact</li></Link>
         </ul>
-      </div>
+      {/* </div> */}
+
+      <button className="mobile-menu-icon" onClick={() => setIsMobile(!isMobile)}>
+        {isMobile ? <i className="fas fa-times"></i> : <i className="fas fa-bars"></i>}
+      </button>
+
     </div>
   )
 }
